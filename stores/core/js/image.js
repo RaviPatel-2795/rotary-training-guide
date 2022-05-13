d0591a66fee6f7e539ec45ff99120203446c4969
RaviPatel-2795 <patel.ravi.2795@gmail.com>
2022-05-13 13:01:52 +0100

    Rotary Training Guide

---
+++ stores/core/js/image.js
@@ -0,0 +1,194 @@
+$(document).ready(function() {
+		  $("#owl-image1").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+		
+$(document).ready(function() {
+		  $("#owl-image2").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400, 
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image3").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image4").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image5").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image6").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image7").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 3,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image8").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+		
+$(document).ready(function() {
+		  $("#owl-image9").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image10").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-image11").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+
+
+$(document).ready(function() {
+		  $("#owl-image12").owlCarousel({
+
+		  navigation : true,
+		  pagination : false,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 400,
+		  items : 4,
+		  });
+		});
+		
+
+$(document).ready(function() {
+		  $("#owl-overlay").owlCarousel({
+
+		  navigation : true,
+		  pagination : true,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 300,
+		  singleItem : true,
+		  autoHeight:true,
+		  });
+		});	
+
+$(document).ready(function() {
+		  $("#owl-usp").owlCarousel({
+
+		  navigation : true,
+		  pagination : true,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 300,
+		  items : 7,
+		  });
+		});
+		
+$(document).ready(function() {
+		  $("#owl-water").owlCarousel({
+
+		  navigation : true,
+		  pagination : true,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 300,
+		  items : 3,
+		  });
+		});
+
+$(document).ready(function() {
+		  $("#owl-service").owlCarousel({
+
+		  navigation : true,
+		  pagination : true,
+		  autoPlay: false,
+		  slideSpeed : 300,
+		  paginationSpeed : 300,
+		  items : 3,
+		  });
+		});		
